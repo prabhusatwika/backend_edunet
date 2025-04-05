8adcee2aaf453fdaa00fd955ca4beb3c9fe31276
prabhusatwika <prabhusatwika25@gmail.com>
2025-04-05 21:44:38 +0530

    made changes for port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const productRoutes = require('./routes/productRoutes');
 const path = require('path')
 const app = express()
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 dotEnv.config();
 
 
@@ -28,6 +28,6 @@ app.listen(PORT, () => {
     console.log(`server started and running at ${PORT}`);
 });
 
-app.use('/home', (req, res) => {
-    res.send(" Welcome to ");
+app.use('/', (req, res) => {
+    res.send(" Welcome to yummy");
 })
